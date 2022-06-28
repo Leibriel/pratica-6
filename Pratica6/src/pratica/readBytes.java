291104c11b2874a670c2eae8b254828431e7286b
Leibriel <101602384+Leibriel@users.noreply.github.com>
2022-06-27 23:30:58 -0300

    Add files via upload

---
+++ Pratica6/src/pratica/readBytes.java
@@ -0,0 +1,14 @@
+package pratica;
+
+public class readBytes {
+	public static void main(String[] args) throws IOException{
+	byte data[] = new byte[10];
+	System.out.println("Digite alguns caracteres: ");
+	System.in.read(data);
+	System.out.print("Sua Entrada de tamanho: " + data.length+ "\n");
+	
+	for(int i=0; i < data.length; i++) {
+		System.out.print((char) data[i]);
+	}
+	}
+}
